cbc95585dd7ffb6fe5fe9d277654c987364ba6bd
Ronaldo Lima <2dlima@2d-Lima.local>
2016-11-07 02:43:02 -0200

    minhas fotos

--- resources/views/includes/galeria.blade.php
+++ resources/views/includes/galeria.blade.php
@@ -3,11 +3,11 @@
 
       <div id="Fotos">
         <article class="thumb">
-          <a href="images/fulls/01.jpg" class="image image-gal"><img src="images/thumbs/01.jpg" alt="Evento" /></a>
+          <a href="images/fulls/_01.jpg" class="image image-gal"><img src="images/thumbs/_01.jpg" alt="Evento" /></a>
           <!-- <h2>Evento </h2> -->
         </article>
         <article class="thumb">
-          <a href="images/fulls/02.jpg" class="image image-gal"><img src="images/thumbs/02.jpg" alt="Evento" /></a>
+          <a href="images/fulls/_02.jpg" class="image image-gal"><img src="images/thumbs/_02.jpg" alt="Evento" /></a>
           <!-- <h2>Evento </h2> -->
         </article>
         <article class="thumb">
